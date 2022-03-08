f99da9197a453f1b59caef8ded5bb1bc32c6e937
OmarElsorogy <gneomar2021@gmail.com>
2022-03-08 21:49:30 +0300

    Update sending emails with SendGrid to user's email

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
-        to: 'ahmedelsayedprog@gmail.com',
+        to: email,
         from: 'ahmedelsayedprog@gmail.com',
         subject: 'This for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
@@ -13,7 +13,7 @@ const sendWelcomeEmail = (email, name) => {
 
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
-        to: 'ahmedelsayedprog@gmail.com',
+        to: email,
         from: 'ahmedelsayedprog@gmail.com',
         subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}. I hope to see you back sometime soon.`
